a7bff6a7fc428327a694d610bdd87e2c4ec2fef6
imorti <imortimer@pivotal.io>
2015-09-09 09:30:48 -0400

    stronger test for initial page load.

--- test/initSpec.js
+++ test/initSpec.js
@@ -1,5 +1,9 @@
 var should = require('should'),
 supertest = require('supertest');
+var request = require('superagent');
+var expect = require('chai').expect;
+
+var baseURL = 'http://localhost:3000';
 
 describe('mhb', function() {
 
@@ -9,5 +13,15 @@ describe('mhb', function() {
 		});
 
 	});
-	
+
+	describe('test initial url', function() {
+			it('should hit index page', function(done) {
+				request.get(baseURL).end(function assert(err, res) {
+					expect(err).to.not.be.ok;
+					expect(res).to.have.property('status', 200);
+					done();
+				})
+			})
+	});
+
 });
