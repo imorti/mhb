0d25247d48275322051582e7ebcf7a37de87ed0d
imorti <imortimer@pivotal.io>
2015-09-23 11:02:53 -0400

    now finding location and nearby locations.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+
+
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    shell: {
+        makeDir: {
+            command: 'xd-singlenode'
+        }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+  //to be able to run shell commands
+  grunt.registerTask('default', ['shell']);
+
+};
