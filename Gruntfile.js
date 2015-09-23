59aef17fde137bdba44fc7be8e74da7da0e9c374
imorti <imortimer@pivotal.io>
2015-09-23 11:13:15 -0400

    added grunt task for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,23 +16,37 @@ module.exports = function(grunt) {
       }
     },
     shell: {
-        makeDir: {
+        springxd: {
             command: 'xd-singlenode'
         }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+        },
+        src: ['test/**/*.js']
+      }
     }
-
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-shell');
-
-
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
 
+
   //to be able to run shell commands
   grunt.registerTask('default', ['shell']);
+
+  grunt.registerTask('default', 'mochaTest');
 
 };
